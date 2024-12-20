a78e58a0039b041d7ab623a9122b87779b6ea1c8
nithissh <129728867+Nithissh683@users.noreply.github.com>
2024-12-20 15:42:55 -0500

    Update URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,5 @@ if st.button('Predict'):
     print('IN button')
     data = json.dumps(user_options,indent =2)
     print(data)
-    r = requests.post('http://127.0.0.1:8000/predict',data = data)
+    r = requests.post('http://68.183.137.170:8002//predict',data = data)
     st.write(r.json())
